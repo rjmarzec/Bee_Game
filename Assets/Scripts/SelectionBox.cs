e6480518c2ef28f1d9c6d502340c678a5a73c7ed
rjmarzec <rjmarzec@umich.edu>
2020-03-08 13:32:55 -0400

    Early Alpha Build

---
+++ Assets/Scripts/SelectionBox.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionBox : MonoBehaviour
+{
+    public GameObject swordGuyPrefab;
+    public GameObject redBulletPrefab;
+
+    Rigidbody2D rigidbody2d;
+    GameObject currentUnit;
+    bool unitAlive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        unitAlive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        unitAlive = currentUnit != null;
+    }
+
+    private void OnMouseDown()
+    {
+        if(!unitAlive)
+        {
+            // Summon a sword_guy
+            if (Input.GetMouseButtonDown(0))
+            {
+                unitAlive = true;
+                currentUnit = Instantiate(swordGuyPrefab, rigidbody2d.position, Quaternion.identity);
+            }
+            // Summon a bow_guy
+            if (Input.GetMouseButtonDown(1))
+            {
+                // Will do later
+            }
+        }
+    }
+}
